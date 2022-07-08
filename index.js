9de4dc1664aefb7ee3cfbce8b1c2a7f68e50cc32
Fathir Ahmed Shishir <mfa.shishir@gmail.com>
2022-07-08 02:17:35 +0200

    singnUp api done

--- index.js
+++ index.js
@@ -3,17 +3,22 @@ import mongoose from "mongoose";
 import cors from "cors";
 import morgan from "morgan";
 const app = express();
-
+import userRouter from "./routes/user.js";
 // mongodb+srv://loveToTravel:<password>@cluster0.wqvbo.mongodb.net/?retryWrites=true&w=majority
 
-const port = 5000;
+
 app.use(morgan("dev"));
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+app.use("/users", userRouter); 
+const MONGODB_URL =`mongodb+srv://loveToTravel:1122334466@cluster0.wqvbo.mongodb.net/Travel_DB?retryWrites=true&w=majority`
+const port = 5000;
 
-app.get('/',(req,res)=>{
-    res.send("hello !")
+mongoose.connect(MONGODB_URL).then(()=>{
+    app.listen(port,()=>{
+        console.log(`server is running on ${port}`)
+    })
+    
 })
-
-app.listen(port,()=>{
-    console.log(`server is running on port ${port}`)
-})
+.catch((error)=>console.log(`${error} did not connect`))
