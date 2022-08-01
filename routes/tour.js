ca196b0f42807f2275677c22b6a0338aab5576a0
Fathir Ahmed Shishir <mfa.shishir@gmail.com>
2022-08-01 15:45:45 +0200

    update and delete api done

--- routes/tour.js
+++ routes/tour.js
@@ -4,13 +4,17 @@ import auth from "../middleware/auth.js";
 
 import {
   createTour,
+  deleteTour,
   getTour,
+  updateTour,
   getTours,
   getToursByUser,
 } from "../controllers/tour.js";
 router.post("/",auth,createTour);
 router.get("/", getTours);
 router.get("/:id", getTour);
+router.delete("/:id", auth, deleteTour);
+router.patch("/:id", auth, updateTour);
 router.get("/userTours/:id", auth, getToursByUser);
 
 
