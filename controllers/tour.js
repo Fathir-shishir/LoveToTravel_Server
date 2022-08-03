5dd288f07e1c3f625a1dca3f5ca945776482406a
Fathir Ahmed Shishir <mfa.shishir@gmail.com>
2022-08-03 14:17:07 +0200

    get tours by title and tag

--- controllers/tour.js
+++ controllers/tour.js
@@ -77,4 +77,24 @@ export const getTour = async (req, res) => {
       res.status(404).json({ message: "Something went wrong" });
     }
   };
+
+  export const getToursBySearch = async (req, res) => {
+    const { searchQuery } = req.query;
+    try {
+      const title = new RegExp(searchQuery, "i");
+      const tours = await TourModal.find({ title });
+      res.json(tours);
+    } catch (error) {
+      res.status(404).json({ message: "Something went wrong" });
+    }
+  };
+  export const getToursByTag = async (req, res) => {
+    const { tag } = req.params;
+    try {
+      const tours = await TourModal.find({ tags: { $in: tag } });
+      res.json(tours);
+    } catch (error) {
+      res.status(404).json({ message: "Something went wrong" });
+    }
+  };
   
--- routes/tour.js
+++ routes/tour.js
@@ -9,10 +9,15 @@ import {
   updateTour,
   getTours,
   getToursByUser,
+  getToursBySearch,
+  getToursByTag,
 } from "../controllers/tour.js";
-router.post("/",auth,createTour);
+router.get("/search", getToursBySearch);
+router.get("/tag/:tag", getToursByTag);
 router.get("/", getTours);
 router.get("/:id", getTour);
+
+router.post("/",auth,createTour);
 router.delete("/:id", auth, deleteTour);
 router.patch("/:id", auth, updateTour);
 router.get("/userTours/:id", auth, getToursByUser);
